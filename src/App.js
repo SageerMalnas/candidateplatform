ce0347f57ba05efd13f9eb4b296d63e8907d61b1
SageerMalnas <sageermalnas78@gmail.com>
2024-05-04 23:02:50 +0530

    Clean Code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,3 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import JobCard from './components/JobCard';
-// import { fetchJobs } from './reducers/jobSlice';
-// // import './styles/JobCard.styles.js'; // Optional, import your custom styles
-
-// function App() {
-//   const dispatch = useDispatch();
-//   const jobs = useSelector(state => state.jobs.jobs);
-//   const loading = useSelector(state => state.jobs.loading);
-//   const error = useSelector(state => state.jobs.error);
-
-//   useEffect(() => {
-//     dispatch(fetchJobs());
-//   }, [dispatch]);
-
-//   return (
-//     <div className="App">
-//       <h1>Jobs</h1>
-//       {loading && <p>Loading jobs...</p>}
-//       {error && <p>Error: {error}</p>}
-//       {jobs.map(job => (
-//         <JobCard key={job.jdUid} jobData={job} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React from 'react';
 import { Container } from '@mui/material';
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,32 +1,3 @@
-// import React, { useEffect } from 'react';
-// import { Grid } from '@mui/material';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchJobs } from '../services/jobApi';
-// import { addJobs } from '../redux/actions/jobActions';
-// import JobCard from './JobCard';
-
-// const JobList = () => {
-//   const dispatch = useDispatch();
-//   const jobs = useSelector(state => state.jobs);
-
-//   useEffect(() => {
-//     fetchJobs()
-//       .then(data => dispatch(addJobs(data.jdList)))
-//       .catch(error => console.error('Error fetching jobs:', error));
-//   }, [dispatch]);
-
-//   return (
-//     <Grid container spacing={2}>
-//       {jobs.map(job => (
-//         <Grid item key={job.jdUid} xs={12} sm={6} md={4}>
-//           <JobCard job={job} />
-//         </Grid>
-//       ))}
-//     </Grid>
-//   );
-// };
-
-// export default JobList;
 
 import React, { useEffect } from 'react';
 import { Grid } from '@mui/material';
