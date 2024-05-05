547bd5f465aa9c88333571005bf4d017b91729ae
SageerMalnas <sageermalnas78@gmail.com>
2024-05-05 11:13:29 +0530

    Card Design completed

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardContent, Typography, Button } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   jobCard: {
-    
-    boxShadow: '1px 7px 9px rgba(1, 1, 8, 0.1)',
+    boxShadow: '1px 7px 15px rgba(1, 1, 8, 0.2)', // Darker shadow
     borderRadius: '25px',
     width: '100%',
     display: 'flex',
-    justifyContent: 'space-between',
-    gap: '20px'
+    flexDirection: 'column',
+    gap: '20px',
+    backgroundColor: '#f9f9f9', // Lighter background color
   },
   companyLogo: {
     width: '60px',
@@ -21,7 +21,6 @@ const useStyles = makeStyles({
     display: 'flex',
     justifyContent: 'left'
   },
- 
   jobDetails: {
     // flex: 1,
     display: 'flex',
@@ -31,29 +30,62 @@ const useStyles = makeStyles({
     width: '100%',
     marginTop: '1rem',
     backgroundColor: '#39efc3',
+    '&:hover': {
+      backgroundColor: '#39efc3', // Remove hover effect
+    }
   },
   refButton:{
     width: '100%',
     marginTop: '1rem',
     backgroundColor: '#2a46b9',
-  }
+    '&:hover': {
+      backgroundColor: '#2a46b9', // Remove hover effect
+    }
+  },
+  headerBox: {
+    boxShadow: '0px 1px 5px rgba(0.2, 0.3, 3, 0.2)', // Darker shadow
+    borderRadius: '20px',
+    backgroundColor: 'white',
+    marginTop: '3px',
+    margin: '1.5rem',
+    padding: '2px',
+    marginLeft:'-5px',
+    width: '55%',
+    fontSize: '0.4rem', // Decrease font size
+    fontFamily: 'Varela Round'
+  },
+  content: {
+    fontFamily: 'Varela Round' // Change font family
+  },
+  ViewMoreButton: {
+    marginTop: '1rem',
+    textAlign:'center'
+  },
 });
 
 const JobCard = ({ job }) => {
   const classes = useStyles();
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
 
   return (
     <Card className={`${classes.jobCard} `} variant="outlined">
-      <CardContent>
+      <CardContent className={classes.content}>
+        <div className={classes.headerBox}>
+          <Typography variant="subtitle1">⌛ Posted 10 days ago {job.applyDeadline}</Typography>
+        </div>
         <div className={classes.infoContainer}>
           <div className={classes.imageClass}>
             {job.logoUrl && <img className={classes.companyLogo} src={job.logoUrl} alt={job.companyName} />}
           </div>
           <div className={classes.jobDetails}>
-            <Typography variant="h6" component="h6">
+            <Typography color="textSecondary" >
               {job.companyName}
             </Typography>
-            <Typography color="textSecondary" gutterBottom>
+            <Typography variant="h6" component="h6" style={{ fontWeight: 'bold' }}>
               {job.jobRole} 
             </Typography>
             <Typography color="textSecondary" gutterBottom>
@@ -65,26 +97,28 @@ const JobCard = ({ job }) => {
           Estimated Salary: {job.minJdSalary} - {job.maxJdSalary} {job.salaryCurrencyCode}
         </Typography>
         <Typography>
-          About Company
+          About Company:
         </Typography>
         <Typography>
           About Us
         </Typography>
-
         <Typography variant="body2" component="p">
-          {job.jobDetailsFromCompany}
-
+          {showFullDescription ? job.jobDetailsFromCompany : `${job.jobDetailsFromCompany.substring(0, 150)}...`}
         </Typography>
+        {job.jobDetailsFromCompany.length > 150 && (
+          <Button className={classes.ViewMoreButton} onClick={toggleDescription} color="primary">
+            {showFullDescription ? 'View Less' : 'View Job'}
+          </Button>
+        )}
         <Typography color="textSecondary" gutterBottom>
           Minimum Experience
           <br/>
            {job.minExp} years
         </Typography>
-        
         <Button className={classes.applyButton} variant="contained" color="primary" href={job.jdLink} target="_blank">
-        ⚡Easy Apply
+          ⚡Easy Apply
         </Button>
-        <Button className={classes.refButton} variant="contained"  target="_blank">
+        <Button className={classes.refButton} variant="contained" href={job.jdLink} target="_blank">
           Unlock Referral Ads
         </Button>
       </CardContent>
